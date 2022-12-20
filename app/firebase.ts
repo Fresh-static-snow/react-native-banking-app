9f70e1a539fc2093ee2eedd95bfbeec776ed2b45
Artem Brovko <artem.brovko@rise-x.io>
2022-12-20 19:07:20 +0200

    change config

--- app/firebase.ts
+++ app/firebase.ts
@@ -8,13 +8,13 @@ import {
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA_vpausTgzefr25_-4J05fpX-WNlLmZFA',
-  authDomain: 'chat-project-1ddf7.firebaseapp.com',
-  projectId: 'chat-project-1ddf7',
-  storageBucket: 'chat-project-1ddf7.appspot.com',
-  messagingSenderId: '136430031390',
-  appId: '1:136430031390:web:d958bf536fd8f3e70dd40f',
-  measurementId: 'G-QB8NSB45YL',
+  apiKey: 'Change me',
+  authDomain: 'Change me',
+  projectId: 'Change me',
+  storageBucket: 'Change me',
+  messagingSenderId: 'Change me',
+  appId: 'Change me',
+  measurementId: 'Change me',
 };
 
 initializeApp(firebaseConfig);
